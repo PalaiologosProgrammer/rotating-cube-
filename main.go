f4759668b1f83b6f7d83d0069850bc37315a490a
PalaiologosProgrammer <teutonic1121@gmail.com>
2026-02-01 14:33:48 +0400

    add image configuration and color palette

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"log"
 	"os"
+	"math"
+	"image"
+	"image/color"
+	"image/gif"
 )
 
 const (
 	fileName = "rotatingCube.gif"
+	width, height = 640, 640
+	offset        = height / 2
 )
 
 var nodes = [][]float64{{-100, -100, -100}, {-100, -100, 100}, {-100, 100, -100}, {-100, 100, 100},
@@ -16,6 +22,11 @@ var edges = [][]int{{0, 1}, {1, 3}, {3, 2}, {2, 0}, {4, 5}, {5, 7}, {7, 6},
 
 
 func main() {
+	fgCol := color.RGBA{0xff, 0x00, 0xff, 0xff}
+	var palette = []color.Color{color.RGBA{0x00, 0x00, 0x00, 0xff}, fgCol}
+	var images []*image.Paletted
+	var delays []int
+
 	imgFile, err := os.Create(fileName)
 	if err != nil {
 		log.Fatal(err)
