1c10dd932d4e714d70f3e83a02bdc5db63095247
PalaiologosProgrammer <teutonic1121@gmail.com>
2026-02-01 14:38:05 +0400

    implement Bresenham's line drawing algorithm

--- main.go
+++ main.go
@@ -20,6 +20,40 @@ var nodes = [][]float64{{-100, -100, -100}, {-100, -100, 100}, {-100, 100, -100}
 var edges = [][]int{{0, 1}, {1, 3}, {3, 2}, {2, 0}, {4, 5}, {5, 7}, {7, 6},
 	{6, 4}, {0, 4}, {1, 5}, {2, 6}, {3, 7}}
 
+func drawLine(x0, y0, x1, y1 int, img *image.Paletted, col color.RGBA) {
+	dx := abs(x1 - x0)
+	dy := abs(y1 - y0)
+	var sx, sy int = -1, -1
+	if x0 < x1 {
+		sx = 1
+	}
+	if y0 < y1 {
+		sy = 1
+	}
+	err := dx - dy
+	for {
+		img.Set(x0, y0, col)
+		if x0 == x1 && y0 == y1 {
+			break
+		}
+		e2 := 2 * err
+		if e2 > -dy {
+			err -= dy
+			x0 += sx
+		}
+		if e2 < dx {
+			err += dx
+			y0 += sy
+		}
+	}
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
 
 func main() {
 	fgCol := color.RGBA{0xff, 0x00, 0xff, 0xff}
